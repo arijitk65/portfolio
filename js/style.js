33f879ea0333398e5a20bfe1c95c7bff35147a12
arijitk65 <39990781+arijitk65@users.noreply.github.com>
2019-10-06 21:09:20 +0530

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,73 @@
+var ml4 = {};
+ml4.opacityIn = [0,1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml4 .letters-1',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-1',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4',
+    opacity: 0,
+    duration: 500,
+    delay: 500
+  });
+
+
+
+
+//    Javascript for changing background to blur    
+
+  
+  var myBlurFunction = function(state) {
+      /* state can be 1 or 0 */
+      var containerElement = document.getElementById('container');
+      var overlayEle = document.getElementById('overlay');
+
+      if (state) {
+          overlayEle.style.display = 'block';
+          containerElement.setAttribute('class', 'blur');
+      } else {
+          overlayEle.style.display = 'none';
+          containerElement.setAttribute('class', null);
+      }
+  };
+
+
